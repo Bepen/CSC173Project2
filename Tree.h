46dc9b98736965f27e57c08ccc5dff46840cb537
Bepen <bepen3@gmail.com>
2017-10-16 10:59:59 -0400

    this one is the 5th ball brother

--- Tree.h
+++ Tree.h
@@ -8,7 +8,6 @@ typedef struct NODE *TREE; struct NODE;
 TREE makeNode0(char x);
 TREE makeNode1(char x, TREE t);
 TREE makeNode4(char x, TREE t1, TREE t2, TREE t3, TREE t4);
-TREE B();
 TREE D();
 TREE N();
 TREE I();
